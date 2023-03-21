2cca972f64e63ed70a0c5eb967d8899e5b4b48ba
Felipe Nogueira <contato.fnog@gmail.com>
2023-03-21 10:43:45 -0300

    Create Link model

---
+++ app/Models/Link.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace fnogcps\Shortly\Models;
+
+use fnogcps\Shortly\DBConnection;
+use PDO;
+
+final class Link
+{
+    private object $db;
+    public function __construct(DBConnection $db = new DBConnection())
+    {
+        $this->db = $db->getInstance();
+    }
+
+    public function create(string $url, string $code): string
+    {
+        $query = $this->db->prepare('
+            INSERT INTO shortly.links VALUES (null, ?, ?)
+        ');
+
+        $query->bindValue(1, $code, PDO::PARAM_STR);
+        $query->bindValue(2, $url, PDO::PARAM_STR);
+        $query->execute();
+        return apache_request_headers()['Referer'] . "s/${code}";
+    }
+
+    public function get(string $code): string
+    {
+        $query = $this->db->prepare('
+            SELECT target FROM shortly.links WHERE code = ?
+        ');
+
+        $query->bindValue(1, $code, PDO::PARAM_STR);
+        $query->execute();
+        return $query->fetchColumn();
+    }
+}
